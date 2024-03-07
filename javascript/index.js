511bffa77008dbe555d4ec2f0929dd449e0cf136
Thiago Sosa <thiagovsosa0112@gmail.com>
2024-03-06 23:23:47 -0300

    termino filtro

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,31 @@
-$(document).ready(function() {
-    //Primer enlace
-    alert('hola mundo')
-    $('.categoria .categoria-item[category="all]').addClass('ct_item-active')
+$(document).ready(function(){
+    
+    $('.categoria .category-item[category="all"]').addClass('ct_item-active');
+
+    
+    $('.category-item').click(function(){
+        var catProduct = $(this).attr('category');
+        console.log(catProduct);
+        $('.category-item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+
+        //ocultar
+        $('.card').hide();
+    
+    
+        //mostrar
+    
+        $('.card[category="'+catProduct+'"]').show();
+    })
+ 
+    $('.category-item[category="all"]').click(function() {
+        $('.card').show();
+    })
+
+
 });
+
 
 
 
